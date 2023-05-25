51648058cc6ec20f62392910929b3908cbc79f53
gusanagy <gustavo.pereira2146@gmail.com>
2023-05-25 18:56:24 -0300

    my first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gusanagy20/stagec_ws/src
+# Build directory: /home/gusanagy20/stagec_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("stage_controller")
